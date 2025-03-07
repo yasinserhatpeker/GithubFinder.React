96a08e93cfc6651ff579423bd9ac35dc03a25183
Yasin <yasinpeker43@gmail.com>
2025-03-07 17:06:59 +0300

    Navbar is added.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export class Navbar extends Component {
   render() {
     return (
-      <nav className=''>
+      <nav className='navbar navbar-dark bg-danger'>
+        <a href="" className='navbar-brand'>
+        <i class="bi bi-github"></i>Github Finder
+        </a>
          
       </nav>
     )
